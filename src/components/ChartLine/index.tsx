0fb4caf3c746ff4ebfc327cf3b43379751c26fec
carlosmedina-repo <devstarclaudio@gmail.com>
2024-02-07 19:40:22 -0500

    ChartLine

---
+++ src/components/ChartLine/index.tsx
@@ -0,0 +1,46 @@
+import { useColorScheme } from "nativewind";
+import { View } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  withSpring,
+} from "react-native-reanimated";
+
+type ChartLineProps = {
+  value?: number;
+};
+
+const ChartLine = ({ value = 0 }: ChartLineProps) => {
+  const { colorScheme } = useColorScheme();
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      width: withSpring(value * 100, {
+        damping: 50,
+        stiffness: 90,
+      }),
+    };
+  }, [value]);
+
+  return (
+    <View className="-1 flex flex-row p-2">
+      <View className="h-2 w-full overflow-hidden rounded-full bg-gray-300 dark:bg-gray-500">
+        <Animated.View
+          className={`h-2 duration-500`}
+          style={[
+            animatedStyle,
+            {
+              backgroundColor:
+                value > 1
+                  ? "#f87171"
+                  : colorScheme === "dark"
+                    ? "#a3e635"
+                    : "#22c55e",
+            },
+          ]}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default ChartLine;
