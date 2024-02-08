2b029cf1ef3fecc5bd8d8edfa201a34a80631586
carlosmedina-repo <devstarclaudio@gmail.com>
2024-02-07 19:50:06 -0500

    useFoodSearch

---
+++ src/query/query/useFoodSearch.ts
@@ -0,0 +1,56 @@
+import { fetchFoodSearch } from "@/src/utils/services";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { useMemo } from "react";
+
+export interface Datum {
+  carbonhydrate: number;
+  protein: number;
+  fat: number;
+  energy: number;
+  name: string;
+  image: string;
+  id: string;
+}
+
+export const useFoodSearch = ({
+  searchTerm,
+  initialPage = 1,
+  initialData,
+}: {
+  searchTerm: string;
+  initialPage?: number;
+  initialData?: Datum[];
+}) => {
+  const { data, isError, fetchNextPage, hasNextPage, isLoading } =
+    useInfiniteQuery<
+      {
+        data: Datum[];
+        page: number;
+      },
+      Error
+    >({
+      queryKey: ["food", "search", searchTerm],
+      staleTime: 1000 * 60 * 60, // 1 hour
+      getNextPageParam: (lastPage) => {
+        return lastPage?.data?.length === 20 ? lastPage.page + 1 : undefined;
+      },
+      initialPageParam: initialPage,
+      queryFn: ({ pageParam }) =>
+        fetchFoodSearch({ searchTerm, page: pageParam }),
+    });
+
+  const results = useMemo(() => {
+    if (initialData) {
+      return initialData;
+    }
+    return data?.pages.flatMap((page) => page.data).filter(Boolean) ?? [];
+  }, [data, initialData]);
+
+  return {
+    results,
+    isLoading,
+    isError,
+    fetchNextPage,
+    hasNextPage,
+  };
+};
