e25ec9dc04c2c15c2eef5d937831fbc187f0a24b
carlosmedina-repo <devstarclaudio@gmail.com>
2024-02-07 19:53:35 -0500

    foods

---
+++ src/screens/foods/index.tsx
@@ -0,0 +1,72 @@
+import EmptyLottieGreen from "@/src/assets/animations/empty-green.json";
+import FoodListItem from "@/src/components/FoodListItem";
+import ScreenView from "@/src/components/ScreenView";
+import SearchBar from "@/src/components/SearchBar";
+import useDebounce from "@/src/hooks/useDebounce";
+import { useFoodSearch } from "@/src/query/hooks/useFoodSearch";
+import { useScrollToTop } from "@react-navigation/native";
+import { FlashList } from "@shopify/flash-list";
+import { useLocalSearchParams } from "expo-router";
+import LottieView from "lottie-react-native";
+import { useEffect, useRef, useState } from "react";
+import { Text, View } from "react-native";
+
+const FoodsScreen = () => {
+  const flashListRef = useRef(null);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const { resultsFromCamera } = useLocalSearchParams();
+
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
+
+  const { results, isLoading, fetchNextPage, hasNextPage, isError } =
+    useFoodSearch({ searchTerm: debouncedSearchTerm, initialData: resultsFromCamera ? JSON.parse(resultsFromCamera as string) : undefined });
+
+  useScrollToTop(flashListRef);
+
+  useEffect(() => {
+    if (resultsFromCamera) {
+      setSearchTerm("Foods detected from camera");
+    }
+  }, [resultsFromCamera]);
+
+  return (
+    <ScreenView>
+      <SearchBar onChangeText={setSearchTerm} />
+      <FlashList
+        ref={flashListRef}
+        data={results}
+        keyExtractor={(food) => food.id}
+        renderItem={({ item }) => <FoodListItem food={item} />}
+        estimatedItemSize={122}
+        onEndReachedThreshold={1.8}
+        onEndReached={() => {
+          if (hasNextPage) {
+            fetchNextPage();
+          }
+        }}
+        ListEmptyComponent={() => {
+          return (
+            <View className="flex-1 items-center justify-center">
+              <LottieView
+                source={EmptyLottieGreen}
+                autoPlay
+                loop
+                style={{ width: 300, height: 300 }}
+              />
+              <Text className="text-lg font-semibold text-gray-500 dark:text-gray-400">
+                {isError
+                  ? "An error occurred while fetching data."
+                  : isLoading
+                    ? "Loading..."
+                    : "No results found."}
+              </Text>
+            </View>
+          );
+        }}
+      />
+    </ScreenView>
+  );
+};
+
+export default FoodsScreen;
