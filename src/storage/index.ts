49bc848063d68ab91265a0b32a21c59493ceec82
carlosmedina-repo <devstarclaudio@gmail.com>
2024-02-07 19:55:46 -0500

    storage

---
+++ src/storage/index.ts
@@ -0,0 +1,30 @@
+import { MMKV } from "react-native-mmkv";
+
+export const STORAGE_KEYS = {
+  USER: "USER",
+  TOKEN: "TOKEN",
+  REMEMBER_ME: "REMEMBER_ME",
+  ONBOARDING_COMPLETED: "ONBOARDING_COMPLETED",
+  LANGUAGE_CODE: "LANGUAGE_CODE",
+};
+
+type StorageKey = keyof typeof STORAGE_KEYS;
+
+const mmkv = new MMKV();
+
+const Storage = {
+  setItem: (key: StorageKey, value: string) => {
+    mmkv.set(STORAGE_KEYS[key], value);
+  },
+  getItem: (key: StorageKey) => {
+    return mmkv.getString(STORAGE_KEYS[key]);
+  },
+  removeItem: (key: StorageKey) => {
+    mmkv.delete(STORAGE_KEYS[key]);
+  },
+  clear: () => {
+    mmkv.clearAll();
+  },
+};
+
+export default Storage;
