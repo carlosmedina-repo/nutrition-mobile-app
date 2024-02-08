9d5e995a1a0cbb665e628cef1f9a2bc302714e33
carlosmedina-repo <devstarclaudio@gmail.com>
2024-02-07 19:46:11 -0500

    constant_api

---
+++ src/constants/Api.ts
@@ -0,0 +1,14 @@
+const BASE_URL = "https://api.nuttrackerapi.com/api/v1";
+
+export const FOOD_SEARCH_API = `${BASE_URL}/food/search`;
+export const LOGIN_API = `${BASE_URL}/auth/login`;
+export const REGISTER_API = `${BASE_URL}/auth/register`;
+export const MEALS_API = `${BASE_URL}/nutrition/list`;
+export const ME_API = `${BASE_URL}/user/me`;
+export const DAILY_TIPS_API = `${BASE_URL}/tip`;
+export const INSERT_MEAL_API = `${BASE_URL}/nutrition`;
+export const WATER_API = `${BASE_URL}/water`;
+export const FOOD_DETAILS_API = `${BASE_URL}/food`;
+export const SUMMARY_API = `${BASE_URL}/nutrition/summaryByDate`;
+export const AI_RECOMMENDATION_API = `${BASE_URL}/nutrition/recommendation`;
+export const SEARCH_FOOD_BY_IMAGE_API = `${BASE_URL}/food/searchFoodFromPhoto`;
