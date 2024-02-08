e7a632a47cd3fba37f529a08d7f4cb606d8d1dc9
carlosmedina-repo <devstarclaudio@gmail.com>
2024-02-07 19:42:49 -0500

    Meals

---
+++ src/components/Meals/types.ts
@@ -0,0 +1,25 @@
+type MealsProps = {
+  title: string;
+  calories: number;
+  mealTime: string;
+  startDate: string;
+  endDate: string;
+};
+
+type MealSectionProps = {
+  mealType: "breakfast" | "lunch" | "dinner";
+  meals: {
+    [key: string]: {
+      id: string;
+      foodName: string;
+      energy: number;
+      lowResImage: string;
+      category_description: string;
+    }[];
+  };
+  energyNeed: number;
+  startDate: string;
+  endDate: string;
+};
+
+export type { MealsProps, MealSectionProps };
